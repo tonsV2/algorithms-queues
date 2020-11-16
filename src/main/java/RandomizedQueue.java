918b134597338ef7f5a46171356c9a330811dbc2
sj <sebastianthegreatful@gmail.com>
2020-11-16 20:51:30 +0700

    Add places holders for RandomizedQueue and Permutation

---
+++ src/main/java/RandomizedQueue.java
@@ -0,0 +1,51 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return true;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return 0;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+        return (Item) new Object();
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+        return (Item) new Object();
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new Iterator<Item>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public Item next() {
+                return null;
+            }
+        };
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+    }
+}
