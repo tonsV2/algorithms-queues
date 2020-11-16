3ebf93579a5ca3d947dc95ba1dddf52c672ab4a8
sj <sebastianthegreatful@gmail.com>
2020-11-16 20:50:52 +0700

    Deque implemented. Exceptions for corner cases missing.

--- src/main/java/Deque.java
+++ src/main/java/Deque.java
@@ -1,60 +1,133 @@
 import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
 
 public class Deque<Item> implements Iterable<Item> {
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    private class Node {
+        Item data;
+        Node prev;
+        Node next;
+
+        Node(Item item) {
+            data = item;
+        }
+    }
+
     // construct an empty deque
     public Deque() {
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-        return false;
+        return size < 1;
     }
 
     // return the number of items on the deque
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the item to the front
     public void addFirst(Item item) {
+        size += 1;
+        Node node = new Node(item);
+
+        node.next = head;
+        node.prev = null;
+
+        if (head != null) {
+            head.prev = node;
+        }
+
+        head = node;
+
+        if (tail == null) {
+            tail = node;
+        }
     }
 
     // add the item to the back
     public void addLast(Item item) {
+        size += 1;
+        Node node = new Node(item);
+
+        node.next = null;
+
+        if (head == null) {
+            node.prev = null;
+            head = node;
+            tail = node;
+            return;
+        }
+
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-        return (Item) new Object();
+        size -= 1;
+        Node node = head;
+        head = head.next;
+        head.prev = null;
+        return node.data;
     }
 
     // remove and return the item from the back
     public Item removeLast() {
-        return (Item) new Object();
+        size -= 1;
+        Node node = tail;
+        tail = node.prev;
+        tail.next = null;
+        return node.data;
     }
 
     // return an iterator over items in order from front to back
-//    public Iterator<Item> iterator() {
-//    }
+    @Override
+    public Iterator<Item> iterator() {
+        return new Iterator<Item>() {
+            Node current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public Item next() {
+                Item data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
 
     // unit testing (required)
     public static void main(String[] args) {
-    }
+        Deque<Integer> integerDeque = new Deque<>();
 
-    @Override
-    public Iterator<Item> iterator() {
-        return null;
-    }
+        integerDeque.addFirst(3);
+        integerDeque.addFirst(2);
+        integerDeque.addFirst(1);
+        integerDeque.addLast(4);
+        integerDeque.addLast(5);
 
-    @Override
-    public void forEach(Consumer<? super Item> action) {
+        System.out.println(integerDeque.isEmpty());
+        System.out.println(integerDeque.size());
 
-    }
+        System.out.println("---");
+        integerDeque.forEach(System.out::println);
 
-    @Override
-    public Spliterator<Item> spliterator() {
-        return null;
+        System.out.println("---");
+        integerDeque.removeFirst();
+        integerDeque.removeFirst();
+        integerDeque.removeLast();
+        integerDeque.removeLast();
+        integerDeque.forEach(System.out::println);
+        System.out.println(integerDeque.isEmpty());
+        System.out.println(integerDeque.size());
     }
 }
