52bbc62adf28178ed33fddcdc47b083f2b44334c
sj <sebastianthegreatful@gmail.com>
2020-11-17 02:37:41 +0700

    Implement Permutation

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -1,5 +1,19 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
-        System.out.println("Hello, Permutation!");
+        int k = Integer.parseInt(args[0]);
+
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            randomizedQueue.enqueue(item);
+        }
+
+        for (int i = 0; i < k; i++) {
+            String item = randomizedQueue.dequeue();
+            StdOut.println(item);
+        }
     }
 }
